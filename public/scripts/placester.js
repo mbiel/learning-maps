454520ce16096c2c47a018d6edf4335f66c8d4b8
Mike Bielagus <bielagus@gmail.com>
2013-08-04 23:10:56 -0400

    Added drawing manager

--- public/scripts/placester.js
+++ public/scripts/placester.js
@@ -11,6 +11,8 @@ function initialize() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var drawingManager = new google.maps.drawing.DrawingManager();
+    drawingManager.setMap(map);
 
     google.maps.event.addListener(map, 'idle', function() {
         bounds = map.getBounds();
